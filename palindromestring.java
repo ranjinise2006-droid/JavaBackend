0cdd00d9e6f74aea65f89837b54d7414951603b6
ranjinise2006-droid <ranjinise2006-droid@users.noreply.github.com>
2026-09-05 12:18:39 +0530

    Add string palindrome checker

---
+++ palindromestring.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class palindromestring {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a string:");
+
+        String str = sc.nextLine();
+        String originalString = str;
+
+        String rev = new StringBuilder(str).reverse().toString();
+
+        if (originalString.equals(rev)) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
+        }
+
+        sc.close();
+    
+}
+
+}
